2c1bb652051fa7cbe5caa0ee42021e9526c9b0c9
Cauê Reis <cauereis846@gmail.com>
2025-11-26 16:21:11 -0300

    MORRA

--- Program.cs
+++ Program.cs
@@ -1,30 +1,46 @@
-﻿List<string> logs = new List<string>{ "INFO", "WARN", "ERROR", "INFO", "ERROR", "ERROR", "CRITICAL", "INFO"};
+﻿List<string> logs = new List<string> { "INFO", "WARN", "ERROR", "INFO", "ERROR", "ERROR", "CRITICAL", "INFO" };
 
 int contadorError = 0;
 int contadorCombo = 0;
 bool alertaDisparado = false;
 
 foreach (string log in logs)
-{   
-    if(contadorError>= 3 || contadorCombo > 20)
+{
+    switch (log)
+    {
+        case "MAINTENANCE":
+            continue;
+
+        case "ERROR":
+            contadorError++;
+            contadorCombo = contadorCombo + 3;
+            break;
+
+        case "WARN":
+            contadorCombo = contadorCombo + 1;
+            contadorError = 0;
+            break;
+
+        case "CRITICAL":
+            contadorCombo = contadorCombo + 10;
+            contadorError = 0;
+            break;
+
+        case "REBOOT":
+            contadorCombo = 0;
+            contadorError = 0;
+            break;
+
+        default:
+            contadorError = 0;
+            break;
+    }
+    if (contadorError >= 3 || contadorCombo > 20)
     {
         alertaDisparado = true;
         break;
     }
-    switch (log)
-    {
-        case "ERROR": contadorError ++; contadorCombo += 3; break;
-
-        case "WARN": contadorCombo =+ 1; contadorError = 0; break;
-
-        case "CRITICAL": contadorCombo =+10; contadorError = 0; break;
-
-        case "REBOOT": contadorCombo = 0; contadorError = 0; break;
-
-        default: break;
-    }
 }
-
 if (alertaDisparado)
     Console.WriteLine("ALERTA DISPARADO");
 else
