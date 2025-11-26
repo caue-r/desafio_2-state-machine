f71997706de77d9394fb185365a50226e5a94dad
Cauê Reis <cauereis846@gmail.com>
2025-11-26 15:32:32 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿List<string> logs = new List<string>{ "INFO", "WARN", "ERROR", "INFO", "ERROR", "ERROR", "CRITICAL", "INFO"};
+
+int contadorError = 0;
+int contadorCombo = 0;
+bool alertaDisparado = false;
+
+foreach (string log in logs)
+{   
+    if(contadorError>= 3 || contadorCombo > 20)
+    {
+        alertaDisparado = true;
+        break;
+    }
+    switch (log)
+    {
+        case "ERROR": contadorError ++; contadorCombo += 3; break;
+
+        case "WARN": contadorCombo =+ 1; contadorError = 0; break;
+
+        case "CRITICAL": contadorCombo =+10; contadorError = 0; break;
+
+        case "REBOOT": contadorCombo = 0; contadorError = 0; break;
+
+        default: break;
+    }
+}
+
+if (alertaDisparado)
+    Console.WriteLine("ALERTA DISPARADO");
+else
+    Console.WriteLine("Sistema Estável");
